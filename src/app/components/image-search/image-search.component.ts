77523f3ab8775a66109643c5d48a550265283f34
Pavel Pogorelov <engineer.pavel@yandex.com>
2020-08-07 02:28:44 +0300

    flickr search

--- src/app/components/image-search/image-search.component.ts
+++ src/app/components/image-search/image-search.component.ts
@@ -3,6 +3,7 @@ import {FormControl} from '@angular/forms';
 import {UrlObject} from '../../models/image.model';
 import {GetPicturesService} from '../../services/get-pictures.service';
 import {Observable} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-image-search',
@@ -15,7 +16,12 @@ export class ImageSearchComponent implements OnInit {
   constructor(private getPicturesService: GetPicturesService) { }
 
   public ngOnInit(): void {
-
+    this.images = this.searchControl.valueChanges.pipe(
+      filter((value) => value && value.length),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((query: string) => this.getPicturesService.getImageUrls(query))
+    );
   }
 
   public search(event: any): void {
