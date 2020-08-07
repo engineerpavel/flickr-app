04e05611a0873a4e6f4c82964b91c05e40de3931
Pavel Pogorelov <engineer.pavel@yandex.com>
2020-08-07 14:27:47 +0300

    unnecessary imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ImageSearchComponent } from './components/image-search/image-search.component';
-import {MatInputModule} from '@angular/material/input';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ImageSearchComponent} from './components/image-search/image-search.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -18,7 +17,6 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule,
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule
